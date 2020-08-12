cb22914457000b90a0998b2e84b53ea63813dc41
Michael Rabon <nobar86@gmail.com>
2020-08-12 17:32:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status = "Transaction rejected. Please check your account balance."
+      @status = "complete"
     else
       @status = "rejected"
       return "complete"
