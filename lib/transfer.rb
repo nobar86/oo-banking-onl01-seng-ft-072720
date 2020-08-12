fe12b2ced2d80bf92bbb12bbb01139352cefcb67
Michael Rabon <nobar86@gmail.com>
2020-08-12 18:08:53 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,22 @@ class Transfer
   end
   
   def execute_transaction
-     
-   end
+    if ((self.valid?) && (self.status == "pending") && (self.sender.balance >= self.amount))
+      self.sender.balance -= self.amount
+      self.receiver.balance += self.amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+  
+  def reverse_transfer
+    if (self.status == "complete")
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      self.status = "reversed"
+    end
+  end
   
 end
