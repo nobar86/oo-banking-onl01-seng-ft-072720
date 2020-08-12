f52d23d4d61329c4fa9f5df3c7e71294789573a8
Michael Rabon <nobar86@gmail.com>
2020-08-12 17:52:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,17 +18,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-
-    elsif @status == "complete"
-      puts "Transaction was already executed"
-    else
-      @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
-      @status = "complete"
-    end
+    
   end
   
 end
