9b185b29e1594083f5a69e9c55afb47e00d132a3
Michael Rabon <nobar86@gmail.com>
2020-08-12 17:58:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,17 +18,7 @@ class Transfer
   end
   
   def execute_transaction
-    if self.sender.valid? && self.sender.balance > self.amount && self.status != "complete"
-
-      self.sender.balance -= amount
-      self.receiver.balance += amount
-      self.status = "complete"
-      
-    else
-       self.status = "rejected"
-        return "Transaction rejected. Please check your account balance."
-      
-    end  
+    
   end
   
 end
